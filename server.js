52d8eeb995e1cba8e4585de846095bcac6651ac9
hilario <157055839+imgaty@users.noreply.github.com>
2026-04-17 08:57:56 +0000

    Project initilization

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
+
+app.use(cors()); // Permite a ligação entre diferentes serviços
+app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + ".jpg");
+    }
+});
+
+const upload = multer({ storage });
+  
+// endpoint para guardar um ficheiro/foto no local que foi designado na criação de 'storage' e depois como upload através de multer
+
+app.post("/upload", upload.single("photo"), (req, res) => {
+    console.log("Foto recebida:", req.file.filename);
+
+    res.json({
+        message: "Foto guardada",
+        file: req.file.filename
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor a correr em http://localhost:3000");
+});
